faa6d21a615307c3b09db5828eb6354ac90e239a
Sam Van Lier <sam.vanlier@hotmail.com>
2020-01-08 16:36:11 +0100

    clean up

--- CA/HuffmanNode.cs
+++ CA/HuffmanNode.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 namespace CA
 {
-    public class HuffmanNode : IComparable
+    internal class HuffmanNode : IComparable
     {
-        public HuffmanNode(HuffmanNode left, HuffmanNode right)
+        internal HuffmanNode(HuffmanNode left, HuffmanNode right)
         {
             Left = left;
             Right = right;
@@ -12,28 +12,28 @@ namespace CA
             IsLeaf = false;
         }
 
-        public HuffmanNode(char symbol, double propability)
+        internal HuffmanNode(char symbol, double propability)
         {
             Symbol = symbol;
             Probability = propability;
             IsLeaf = true;
         }
 
-        public HuffmanNode Left { get; set; }
-        public HuffmanNode Right { get; set; }
-        public double Probability { get; set; }
+        internal HuffmanNode Left { get; set; }
+        internal HuffmanNode Right { get; set; }
+        internal double Probability { get; set; }
 
-        public bool IsLeaf { get; set; }
-        public char Symbol { get; set; }
+        internal bool IsLeaf { get; set; }
+        internal char Symbol { get; set; }
 
-        public int CompareTo(object obj)
+        int IComparable.CompareTo(object obj)
         {
             var temp = (HuffmanNode)obj;
 
             return Probability.CompareTo(temp.Probability);
         }
 
-        public void Print(string code = "")
+        internal void Print(string code = "")
         {
             if (IsLeaf)
                 Console.WriteLine("leaf - " + Symbol + " - " + code);
@@ -44,6 +44,5 @@ namespace CA
                 this.Right.Print(code + "1");
             }
         }
-
     }
 }
